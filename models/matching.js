7f467d44213c7a8bc0c6a3202ad4ab2c940d3dc1
ShineYeon <chlgmldus2@naver.com>
2022-06-06 21:18:08 +0900

    matching table 수정

--- models/matching.js
+++ models/matching.js
@@ -3,84 +3,11 @@ module.exports = function (sequelize, DataTypes) {
     "matching",
     {
       //테이블 이름임
-      field1: {
-        type: DataTypes.STRING(40),
-        allowNull: false,
-        primaryKey: true,
-      },
-      relatedingredient1: {
+      recipe_ingredient: {
         type: DataTypes.STRING(40),
         allowNull: true,
       },
-      relatedingredient2: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient3: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient4: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient5: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient6: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient7: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient8: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient9: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient10: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient11: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient12: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient13: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient14: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient15: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient16: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient17: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient18: {
-        type: DataTypes.STRING(40),
-        allowNull: true,
-      },
-      relatedingredient19: {
+      db_ingredient: {
         type: DataTypes.STRING(40),
         allowNull: true,
       },
