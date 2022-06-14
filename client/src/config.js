37c0e72e894a6a7b977956075faec21fc3e4d62d
ShineYeon <chlgmldus2@naver.com>
2022-06-14 20:02:26 +0900

    수정

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://iwipwq-nest.herokuapp.com/api/",
+  baseURL: "https://mungnyangapp.herokuapp.com/mungnyang-server/",
 });
